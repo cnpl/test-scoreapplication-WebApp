762f168978a679fd2b96a31738860d03635ac453
cnpl <bhuvana@cnpl.net>
2025-09-20 18:45:47 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from './core/guards/auth.guard'; // <-- Import the guard
 const routes: Routes = [
   {
     path: 'auth',
