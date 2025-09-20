2602597d1772cb03572772d8751b3cc89ff3a331
cnpl <bhuvana@cnpl.net>
2025-09-20 18:48:26 +0530

    Create scores-routing.module.ts

---
+++ src/app/features/scores/services/scores-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ScoreListComponent } from './components/score-list/score-list.component';
+
+const routes: Routes = [
+  { path: '', component: ScoreListComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ScoresRoutingModule { }
