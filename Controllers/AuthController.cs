fe2a3f893a37fe71d1997f59af73bfc4ba3beaa0
cnpl <bhuvana@cnpl.net>
2025-09-20 18:09:54 +0530

    Update AuthController.cs

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,1 +1,23 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 
+namespace ScoreTracker.WebApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpGet("microsoft-login")]
+    public IActionResult MicrosoftLogin()
+    {
+        var properties = new AuthenticationProperties { RedirectUri = "/" };
+        return Challenge(properties, "Saml2");
+    }
+
+    [HttpGet("google-login")]
+    public IActionResult GoogleLogin()
+    {
+        var properties = new AuthenticationProperties { RedirectUri = "/" };
+        return Challenge(properties, "Google");
+    }
+}
