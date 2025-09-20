07d957cd3a4c9b26d9c9a2a825dc0bd3242df700
cnpl <bhuvana@cnpl.net>
2025-09-20 19:10:54 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard'; // <-- Import the guard
+import { AdminGuard } from './core/guards/admin.guard'; // <-- Import the new guard
 const routes: Routes = [
   {
     path: 'auth',
@@ -11,6 +12,11 @@ const routes: Routes = [
     // We will create this module next
     // canActivate: [AuthGuard], // We will add a guard later
     loadChildren: () => import('./features/scores/scores.module').then(m => m.ScoresModule)
+  },
+   {
+    path: 'admin',
+    canActivate: [AdminGuard], // <-- Apply the AdminGuard
+    loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule)
   },
   { path: '', redirectTo: '/scores', pathMatch: 'full' },
   { path: '**', redirectTo: '/scores' } // Redirect unknown paths
