36b39e2a93b2b7986dbd48f3d6a7935d9cdf3cfa
cnpl <bhuvana@cnpl.net>
2025-09-20 18:12:00 +0530

    Create api.service.ts

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) { }
+
+  post<T>(path: string, body: object = {}) {
+    return this.http.post<T>(
+      `${environment.apiUrl}${path}`,
+      JSON.stringify(body),
+      {
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true // CRITICAL: This sends the session cookie with every request
+      }
+    );
+  }
+
+  get<T>(path: string) {
+    return this.http.get<T>(`${environment.apiUrl}${path}`, { withCredentials: true });
+  }
+}
